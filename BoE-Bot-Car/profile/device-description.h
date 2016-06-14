99f23dcea06deb4a3d2acab50240c221ed31a9de
Joe George <jgeorge@afero.io>
2016-06-14 09:00:57 -0700

    afCar profile for boebot project

---
+++ BoE-Bot-Car/profile/device-description.h
@@ -0,0 +1,60 @@
+/*
+ * Afero Device Profile header file
+ * Device Description:		13306bc7-adc0-4194-83be-eba054dfb3d5
+ * Schema Version:	2
+ */
+
+
+//region Service ID 1
+// Attribute Servo1
+#define AF_SERVO1                                              1
+#define AF_SERVO1_SZ                                           2
+
+// Attribute Servo2
+#define AF_SERVO2                                              2
+#define AF_SERVO2_SZ                                           2
+
+// Attribute Steer_Attr
+#define AF_STEER_ATTR                                          3
+#define AF_STEER_ATTR_SZ                                       2
+
+// Attribute Accel_Attr
+#define AF_ACCEL_ATTR                                          4
+#define AF_ACCEL_ATTR_SZ                                       2
+
+// Attribute Transmission_Attr
+#define AF_TRANSMISSION_ATTR                                   5
+#define AF_TRANSMISSION_ATTR_SZ                                2
+
+// Attribute Bootloader Version
+#define AF_BOOTLOADER_VERSION                               2001
+#define AF_BOOTLOADER_VERSION_SZ                               8
+
+// Attribute Softdevice Version
+#define AF_SOFTDEVICE_VERSION                               2002
+#define AF_SOFTDEVICE_VERSION_SZ                               8
+
+// Attribute Application Version
+#define AF_APPLICATION_VERSION                              2003
+#define AF_APPLICATION_VERSION_SZ                              8
+
+// Attribute Profile Version
+#define AF_PROFILE_VERSION                                  2004
+#define AF_PROFILE_VERSION_SZ                                  8
+
+// Attribute Security Enabled
+#define AF_SECURITY_ENABLED                                60000
+#define AF_SECURITY_ENABLED_SZ                                 1
+
+// Attribute Reboot Reason
+#define AF_REBOOT_REASON                                   65019
+#define AF_REBOOT_REASON_SZ                                   64
+
+// Attribute BLE Comms
+#define AF_BLE_COMMS                                       65020
+#define AF_BLE_COMMS_SZ                                       12
+
+// Attribute SPI Enabled
+#define AF_SPI_ENABLED                                     65021
+#define AF_SPI_ENABLED_SZ                                      1
+//endregion
